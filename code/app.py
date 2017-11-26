1ac625f4b552a02cff790c03f2060b0f1ae25f5a
Piotr Turlej <piotr.turlej@me.com>
2017-11-26 13:39:42 +0100

    New post end point

--- code/app.py
+++ code/app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+items = []
 
-class Student(Resource):
-    def get(self,name):
-        return {'student' : name}
+class Item(Resource):
+    def get(self, name):
+        for i in items:
+            if i['name'] == name:
+                return i
+        return {'name': None} 
 
-api.add_resource(Student, '/student/<string:name>')
+
+    def post(self, name):
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item , 201
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 app.run(port=5000)
 
