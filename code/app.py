8dc3a635bb55f9fd748f125f7d6201a2f27c3757
PiotrFrugoton <piotr.turlej@fr>
2017-11-27 20:24:59 +0100

    New parser added

--- code/app.py
+++ code/app.py
@@ -1,30 +1,60 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'secret123'
 api = Api(app)
+jwt = JWT(app, authenticate, identity)
+
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         return next(filter(lambda x: x['name'] == name, items), {'item': None})
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
-            return { 'message ', "An item with name '{}' already exist".format(name)} , 400
+            return { 'message' : 'An item with name {0} already exist'.format(name)}, 400
+
+
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item , 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda item: item['name'] != name, items))
+        return {'message': 'Item deleted'}, 400
+
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help="This field cannot be left blank"
+        )
+        data = parser.parse_args()
+        
+        item = next(filter(lambda item: item['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class ItemList(Resource):
     def get(self):
-        return {'items': items}
+        return {'items': items}, 200
 
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-app.run(port=5000)
+app.run(port=5000, debug=True)
 
--- code/security.py
+++ code/security.py
@@ -1,13 +1,17 @@
+from werkzeug.security import safe_str_cmp
 from user import User
 
 users = [
     User(1, "Piotr", "asdf")
 ]
 
-username_mapping = { item.username: item for item in users }
-userid_mapping = { item.id: item for item in users }
+username_mapping = {item.username: item for item in users}
+userid_mapping = {item.id: item for item in users}
 
-
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and safe_str_cmp(user.password, password):
+        return user
 
 def identity(payload):
     user_id = payload['identity']
