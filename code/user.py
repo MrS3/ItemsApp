9d5f5872688a07760aa9342c39826728d26958e3
Piotr Turlej <piotr.turlej@me.com>
2017-11-30 20:29:20 +0100

    Database query improvememnts

--- code/user.py
+++ code/user.py
@@ -11,15 +11,17 @@ class User:
 
     @classmethod
     def find_by_username(cls,username):
-        return cls(*selectItem("SELECT * FROM users WHERE username=?", username))
+        row = selectItem("SELECT * FROM users WHERE username=?", username)
+        return cls(*row) if row else None
 
     @classmethod
     def find_by_id(cls,_id):
-        return cls(*selectItem("SELECT * FROM users WHERE id=?", _id))
+        row = selectItem("SELECT * FROM users WHERE id=?", _id)
+        return cls(*row) if row else None
 
 
 class UserRegister(Resource):
-
+    
     parser = reqparse.RequestParser()
     parser.add_argument('username',
         type = str,
@@ -35,15 +37,10 @@ class UserRegister(Resource):
 
     def post(self):
         data = UserRegister.parser.parse_args()
-        
+
         if User.find_by_username(data['username']):
             return {'message': 'A user with that username already exist'}, 400
 
-        connection = sqlite3.connect('data.db')
-        currsor = connection.cursor()
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        result = currsor.execute(query, (data['username'], data['password']))
-        connection.commit()
-        connection.close()
+        insertItem("INSERT INTO users VALUES (NULL, ?, ?)", (data['username'], data['password']))
         return {'message' : 'User created succesfully'}
 
