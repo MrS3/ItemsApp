183559be8807bc2b32f6cd21bfa0383a19214fc1
Piotr Turlej <piotr.turlej@me.com>
2017-12-05 19:21:05 +0100

    db model added

--- code/database.py
+++ code/database.py
@@ -1,4 +1,7 @@
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def selectItem(query, *args):
      connection = sqlite3.connect('data.db')
--- code/models/item_model.py
+++ code/models/item_model.py
@@ -1,6 +1,13 @@
 from database import insertItem, selectItem, selectAllItems
+from database import db
 
-class ItemModel:
+class ItemModel(db.Model):
+
+    __tablename__ = items
+    id = db.Columnt(db.Integer, 'primary_key=True')
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+
     def __init__(self, name, price):
         self.name = name
         self.price = price
